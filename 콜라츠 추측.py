5c67889be21bd33559db167234e7361580489305
woorud96 <woorud96@gmail.com>
2020-01-25 22:45:38 +0900

    Day 4

---
+++ 콜라츠 추측.py
@@ -0,0 +1,24 @@
+def solution(num):
+    answer = 0
+
+    while True :
+        if num == 1:
+            answer = 0
+
+        elif num % 2 == 0 :
+            num = num / 2
+            answer += 1
+
+        else:
+            num = num*3 + 1
+            answer += 1
+
+        if num == 1 :
+            break
+
+    if answer > 500 :
+        return -1
+    else:
+        return answer
+
+print(solution(1))
