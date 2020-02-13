b400728a641105cf4d384e42a3157477419d6f9f
woorud96 <woorud96@gmail.com>
2020-02-13 23:23:23 +0900

    Day 22

---
+++ 가장 큰 수.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key = lambda x : x * 3, reverse = True) # 1000이하의 자연수이기 때문에 *3 을 통해 정렬
+    return str(int(''.join(numbers)))
+
+print(solution([6, 10, 2]))
