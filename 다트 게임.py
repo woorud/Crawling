592d331b0f01428f48e87eb1e82491a89ab222fd
woorud96 <woorud96@gmail.com>
2020-03-07 22:05:11 +0900

    Day 46

--- 다트 게임.py
+++ 다트 게임.py
@@ -1,32 +1,35 @@
 def solution(dartResult):
+    dartResult = dartResult.replace('10', 'a')
     dR = list(dartResult)
     score = []
 
     for i in dR:
         if i.isdigit():
             score.append(int(i))
-            if score[-1] == 0 and score[-2] == 1:
-                score[-2] = 10
-                score.pop(-1)
+        elif i == 'a':
+            score.append(10)
         elif i.isalpha():
-            if i == "S":
+            if i == 'S':
                 score[-1] = score[-1] ** 1
-            elif i == "D":
+            elif i == 'D':
                 score[-1] = score[-1] ** 2
-            else:
+            elif i == 'T':
                 score[-1] = score[-1] ** 3
         else:
-            if i == "*":
+            if i == '#':
+                score[-1] = score[-1] * (-1)
+            else:
                 if len(score) == 1:
                     score[-1] = score[-1] * 2
                 else:
                     score[-1] = score[-1] * 2
                     score[-2] = score[-2] * 2
-            else:
-                score[-1] = score[-1] * (-1)
 
     return sum(score)
 
+
+
+print(solution('1S2D*3T'))
 print(solution('10S2D*3T'))
 print(solution('1S*2T*3S'))
 print(solution('1D#2S*3S'))
