fbc16f19e08caa07570ef55c7e232730f546dfbd
woorud96 <woorud96@gmail.com>
2020-03-11 11:01:33 +0900

    Day 50

--- 다리를 건너는 트럭.py
+++ 다리를 건너는 트럭.py
@@ -1,23 +1,23 @@
 def solution(bridge_length, weight, truck_weights):
     passing = [0] * bridge_length
     sec = 0
-    sq = 0
+    stay = 0
 
-    while truck_weights :
+    while truck_weights:
         sec += 1
-        x = passing.pop(0)
-        if x != 0:
-            sq -= x
+        out = passing.pop(0)
+        if out != 0:
+            stay -= out
 
-        if truck_weights:
-            if sq + truck_weights[0] <= weight:
-                a = truck_weights.pop(0)
-                passing.append(a)
-                sq += a
-            else:
-                passing.append(0)
-
+        if stay + truck_weights[0] <= weight:
+            ing = truck_weights.pop(0)
+            passing.append(ing)
+            if ing != 0:
+                stay += ing
+        else:
+            passing.append(0)
 
     return sec + bridge_length
+
 print(solution(2, 10, [7, 4, 5, 6]))
 print(solution(100, 100, [10]))
