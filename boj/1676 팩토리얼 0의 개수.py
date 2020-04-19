936a68cf2905f822d2f946c901a56d505ba358d4
woorud96 <woorud96@gmail.com>
2020-04-19 23:11:22 +0900

    Day 88

--- boj/1676 팩토리얼 0의 개수.py
+++ boj/1676 팩토리얼 0의 개수.py
@@ -1,17 +1,12 @@
-def fac(n):
-    if n == 1:
-        return 1
-    else:
-        return n*fac(n-1)
-
 n = int(input())
-num = list(str(fac(n)))
 cnt = 0
+fac = 1
 
-for i in num[::-1]:
+for i in range(1, n+1):
+    fac = fac * i
+for i in str(fac)[::-1]:
     if i == '0':
         cnt += 1
     else:
-        print(cnt)
         break
-
+print(cnt)
