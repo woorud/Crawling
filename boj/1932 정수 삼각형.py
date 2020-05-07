96d8a3c9dad603abee11085f17e03121dbbeaf5d
woorud96 <woorud96@gmail.com>
2020-05-07 14:16:37 +0900

    Day 107

--- boj/1932 정수 삼각형.py
+++ boj/1932 정수 삼각형.py
@@ -2,7 +2,6 @@ n = int(input())
 triangle = [list(map(int, input().split())) for i in range(n)]
 dp = [[0]*i for i in range(1, n+1)]
 dp[0][0] = triangle[0][0]
-
 for i in range(n):
     for j in range(i+1):
         if j == 0:
