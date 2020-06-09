140545f41cc6e0272c2f89695b64be397c4f320d
woorud96 <woorud96@gmail.com>
2020-06-09 14:12:58 +0900

    Day 140

---
+++ programmers/정수 삼각형.py
@@ -0,0 +1,18 @@
+def solution(triangle):
+    dp = [[0]*i for i in range(1, len(triangle)+1)]
+    dp[0][0] = triangle[0][0]
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+            elif j < i:
+                dp[i][j] = max(dp[i-1][j-1]+triangle[i][j], dp[i-1][j]+triangle[i][j])
+            else:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+
+
+
+
+    return max(dp[-1])
+
+print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
