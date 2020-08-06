714b32de374237e8df0e16a7851cc50146b15de9
woorud96 <woorud96@gmail.com>
2020-08-06 23:58:10 +0900

    Day 184

---
+++ boj/15651 N과 M (3).py
@@ -0,0 +1,27 @@
+N, M = map(int, input().split())
+
+num_list = [i + 1 for i in range(N)]
+check_list = [False] * N
+
+arr = []
+
+def dfs(cnt):
+    # 주어진 개수만큼 채워지면 출력한다
+    if(cnt == M):
+        print(*arr)
+        return
+    
+    for i in range(0, N):
+        if(check_list[i]):
+            continue
+        
+        check_list[i] = True
+        arr.append(num_list[i])
+        dfs(cnt + 1)
+        arr.pop()
+        
+        for j in range(i + 1, N):
+            check_list[j] = False
+            
+        
+dfs(0)
